50b1fd85270eff1cf7302717556365c5e5f3b2b8
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-19 19:32:26 +0200

    listas

---
+++ m2_python/Variables/listas.py
@@ -0,0 +1,30 @@
+numero = 8
+numeros = [1,2,3,4,5,6,7,8]
+
+print(numero)
+print(numeros)
+
+nifs = ['1111111', '2222222', '3333333']
+print(nifs)
+
+validos = [True, False, True, False]
+print(validos)
+
+lista_heterogenea = [1, 'MiguelAngel', True, 20.20]
+print(lista_heterogenea)
+
+precios = [20.50, 10.50, 5.50]
+print(precios)
+
+telefonos = ['680924086','680924087', '680924087' ]
+emails =['user1@gamil.com', 'user2@gmail.com', 'user3@gmail.com']
+contactos = [telefonos, emails]
+
+print(contactos)
+print(contactos[0][0])
+print(contactos[0][1])
+print(contactos[0][2])
+
+print(contactos[1][0])
+print(contactos[1][1])
+print(contactos[1][2])

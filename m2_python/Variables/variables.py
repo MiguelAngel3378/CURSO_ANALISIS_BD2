ec3c985571a77fa26c3a318c4890f0d237158808
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-19 18:38:52 +0200

    Variables ydatos

---
+++ m2_python/Variables/variables.py
@@ -0,0 +1,21 @@
+print('hola coca cola')
+print('hello world')
+
+print("You've successfully run some Python code")
+print("Congratulations!")
+
+nombre = 'miguel angel'
+apellido = 'suarez'
+full_namme = 'miguel angel suarez'
+edad = 46
+
+print(nombre)
+print(apellido)
+print(full_namme)
+print(nombre +' '+ apellido)
+print('hola, soy= ' + nombre +' , mi edad es:' + str(edad))
+print('hola, soy= ' + nombre +' , mi edad es:' ,edad)
+
+
+
+

77d7240e13b5b4e24c7b48ebbc2d9bf5aa64116b
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-19 20:09:47 +0200

    upload

---
+++ m2_python/Variables/diccionarios.py
@@ -0,0 +1,12 @@
+
+alumnos = {
+    '1212121212A': 'Robaeto Carlos',
+    '1313131313B': 'Ricardo Nontaner',
+    '1414141414C': 'Ramirez Pillo',
+    '1515151515D': 'Juan Lolo'
+}
+
+tabla_alumnos = {
+    1: ('Roberto', 'Apellido', 'user1@gmail.com', 30, False),
+    2: ('Robert02', 'Apellido2', 'user2@gmail.com', 40, True)
+}
---
+++ m2_python/Variables/tuplas.py
@@ -0,0 +1,8 @@
+
+employe1 = (1,'roberto','carlos', 'robertocarlos@gmail.com', 35, False)
+employe2 = (2,'ricardo','montaner', 'ricardomontaner@gmail.com', 45, True)
+
+table_employees = [
+    (1,'roberto','carlos', 'robertocarlos@gmail.com', 35, False),
+    (2,'ricardo','montaner', 'ricardomontaner@gmail.com', 45, True)
+]

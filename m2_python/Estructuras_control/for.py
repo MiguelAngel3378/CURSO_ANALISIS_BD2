5a2fd21000ef7a10ffba30456a7594e2ea57e1f0
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-20 17:21:11 +0200

    upload

---
+++ m2_python/Estructuras_control/for.py
@@ -0,0 +1,15 @@
+
+names = ['juan', 'malala', 'rosa']
+print('rosa' in names)
+
+for name in names:
+    print(name)
+    
+numeros = [x for x in range(1, 11)]
+print(numeros)
+
+cuadrados = [x**2 for x in range(1,11)]
+print(cuadrados)
+
+pares = [x for x in range(1,21) if x % 2 == 0]
+print(pares)
---
+++ m2_python/Estructuras_control/if.py
@@ -0,0 +1,21 @@
+
+
+edad = 17
+
+if edad >=18:
+    print('es mayor de edad')
+else:
+    print('es menor de edad')
+
+print('es mayor de edad') if edad >=18 else print('es menor de edad')
+    
+    
+altura = 180
+
+if altura >= 160 and altura <= 170:
+    print('grupo1')
+elif altura > 170 and altura <= 180:
+    print('grupo 2')
+else:
+    print('grupo 3')
+    

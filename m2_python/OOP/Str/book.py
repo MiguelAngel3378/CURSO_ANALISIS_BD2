65e553a4a87a9e9f4400b044aef9265efb2bfe9e
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-25 20:19:26 +0200

    updated

--- m2_python/OOP/Str/book.py
+++ m2_python/OOP/Str/book.py
@@ -1,11 +1,19 @@
-
-class Book:
-    def __init__(self, title, num_page):
-        self.title = title
-        self.num_page = num_page
+class Author:
+    def __init__(self, id, nif, email, year, city, num_books=0):
+        self.id = id
+        self.nif = nif
+        self.email = email
+        self.year = year
+        self.city = city
+        self.num_books = num_books
         
     def __str__(self):
-        return f'title={self.title}, num_pages={self.num_pages}'
+        return f"Author con id = {self.id} " \
+        f"nif = {self.nif} " \
+        f"email = {self.email} " \
+        f"year = {self.year} " \
+        f"city = {self.city} " \
+        f"num_books = {self.num_books}"
     
-book1 = Book('book1, 340')
-print(book1)
+author1 = Author(1, '2322323R', 'author1@email.com', 2000, 'Madrid')
+print(author1)

19ae7d5ac5b33d9bc99c8180f1f5fc2f86b7dae2
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-26 18:25:23 +0200

    today

--- m2_python/Lectura consola/main.py
+++ m2_python/Lectura consola/main.py
@@ -5,5 +5,9 @@ edad = int(input("Introduce edad\n"))
 
 salary = float(input("Introduce salario\n"))
 
+apto = bool(int(input("0 Es no apto y 1 es apto\n")))
+
+fecha_nacimiento = input("Introduce fecha de nacimiento (YYYY-MM-dd) (1990-07-20) \n")
+
 print("fin")
 

c76efba7a81bac2eb6c3c6f5282be2367633dcb1
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-26 18:51:43 +0200

    now

--- m2_python/Lectura consola/main.py
+++ m2_python/Lectura consola/main.py
@@ -1,13 +1,19 @@
 
-nombre = input("Introdusca su nombre\n")
+# nombre = input("Introdusca su nombre\n")
 
-edad = int(input("Introduce edad\n"))
+# edad = int(input("Introduce edad\n"))
 
-salary = float(input("Introduce salario\n"))
+# salary = float(input("Introduce salario\n"))
 
-apto = bool(int(input("0 Es no apto y 1 es apto\n")))
+# apto = bool(int(input("0 Es no apto y 1 es apto\n")))
 
-fecha_nacimiento = input("Introduce fecha de nacimiento (YYYY-MM-dd) (1990-07-20) \n")
+# fecha_nacimiento = input("Introduce fecha de nacimiento (YYYY-MM-dd) (1990-07-20) \n")
 
-print("fin")
+fecha_nacimiento_str = input("Introduce fecha de nacimiento (dd-MM-YYYY) (03/11/1993) \n")
+year
+month
+day
+datetime.date[1993, 1, 28]
+
+
 

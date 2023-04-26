e76df3752b30aafaa32efe21b9aacd45bd461e3a
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-26 17:01:46 +0200

    uptoday

---
+++ m2_python/OOP/Str/main.py
@@ -0,0 +1,9 @@
+
+import offer
+
+oferta1 = offer.Jobffer('Analista_datos', 'Adecco', 20000, 'Hibrido', 1)
+oferta2 = offer.Jobffer('Database_controler', 'IBM', 25000, 'Presencial', 2)
+
+print(oferta1)
+print(oferta2)
+

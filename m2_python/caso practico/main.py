d322a359f2495eae38ae355e00e3db585a55f068
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-26 20:18:07 +0200

    now

---
+++ m2_python/caso practico/main.py
@@ -0,0 +1,48 @@
+
+"""
+
+*** Para hacer en clase: 
+
+1. Travel.py:
+clase travel 
+    id, city_from, city_to
+    
+2. Traveldatabase.py:
+clase traveldatabase
+    []
+    def findall
+    def findById
+    def findByCityFromAndCityTo
+    def insert
+    def update      
+    def delete
+    
+3. app.py
+    pruebe todas las operaciones con Travaldatabase
+    leer de imnput los datos para crear objetos Travel
+    
+*** Para entregar:
+
+Base de datos de incendios
+
+1.fire.py
+class Fire
+    id, street, city, province, level, date_from, date_to, active, cause
+
+2.firedatabase.py
+class Firedatabase
+    []
+    def findall
+    def findById
+    def findByActiveTrue
+    def findByCause(cause)
+    def insert(fire)
+    def update(fire)      
+    def delete(fire)
+    
+3.app.py
+imprimir menu que permita por consola ejecutar los metodos de FireDataBase
+leer de consola leer datos para crear incendios, filtrar, etc.
+que el app incerte demo en el arranque
+
+"""

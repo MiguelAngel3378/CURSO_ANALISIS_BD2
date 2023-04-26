a56931c146c14a556454615a8bc17fa56571a7cc
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-26 19:49:34 +0200

    uptoday

--- m2_python/Lectura consola/main.py
+++ m2_python/Lectura consola/main.py
@@ -8,14 +8,16 @@
 # apto = bool(int(input("0 Es no apto y 1 es apto\n")))
 
 # fecha_nacimiento = input("Introduce fecha de nacimiento (YYYY-MM-dd) (1990-07-20) \n")
+import datetime
 
-fecha_nacimiento_str = input("Introduce fecha de nacimiento (dd/MM/YYYY) (03/11/1993) \n")
-    
-year
-month
-day
+fecha_nacimiento_str = input("Introduce fecha nacimiento (dd/MM/YYYY) (03/11/1991) \n")
+fecha_array = fecha_nacimiento_str.split("/")
+year = int(fecha_array[2])
+month = int(fecha_array[1])
+day = int(fecha_array[0])
+fecha_final = datetime.date(year, month, day)
 
-datetime.date[1993, 1, 28]
+print("fin")
 
 
 

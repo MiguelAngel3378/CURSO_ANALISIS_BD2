47509e8396ff680ead0f841a83cb6ab01f0aefe5
Mike6969A <miguelsuarez3378@gmail.com>
2023-04-27 17:38:09 +0200

    updated

---
+++ m2_python/caso practico/Para entregar/fire.py
@@ -0,0 +1,13 @@
+
+
+class Fire:
+    def __init__(sel, id, street, city, province,  level, date_from, date_to, active, cause);
+        self.id = id
+        self.street = street
+        self.city = city
+        self.province = province
+        self.level = level
+        self.date_from = date_from
+        self.dato_to = date_to
+        self.active = active
+        self.cause = cause
